9a8cf5d2c22409729de5ba9b8feae443f1ab07f1
Fayas <fayazismael76@gmail.com>
2025-04-04 10:12:31 +0530

    new repo

--- public/script.js
+++ public/script.js
@@ -1,6 +1,9 @@
-console.log("successfully received data");
-async function getdata(){
-    const res = await fetch("/getdata");
+// 
+async function getdata() {
+    
+    const res = await fetch("http://localhost:3000/tasks");
+    console.log(res);
+
     const data = await res.json();
     console.log(data);
 
@@ -8,20 +11,175 @@ async function getdata(){
     data.forEach(e => {
         str+=`
         <div class="list">
-            <div class="task">${e.task}</div>
+            <div class="task"><input type="checkbox" ${e.task?"checked":""}/><span>${e.task}</span></div>
+
             <div class="btn">
-                <button class="button" type="submit" onclick="updatefn('${e._id}', '${e.task}')">Edit</button>
-                <a href="/delete/${e._id}"><button class="button">Delete</button></a>
+                <button class="button" onclick="updatefn('${e._id}', '${e.task}')">Edit</button>
+                <button class="button" onClick='deleteTask("${e._id}")'>Delete</button>
+
             </div>
+
         </div>`
     })
     document.getElementById("menu").innerHTML=str;
 }
 getdata()
 
-function updatefn(id,task){
-    document.getElementById("task").value = task;
-    document.getElementById("addbtn").textContent = "Update";
-    const form = document.getElementById("form")
-    form.action = `/update/${id}`;
+async function updatedata() {
+    
 }
+
+
+
+
+
+current_id=null
+ 
+function updatefn(id, task){
+    document.getElementById("task").value = task
+    document.getElementById("addbtn").textContent = "Update"
+    current_id = id
+}
+
+document.getElementById('form').addEventListener('submit', async (e)=>{
+    e.preventDefault()
+    let task = document.getElementById("task").value
+    if(current_id){
+        await updateTask(current_id,task)
+    }
+    else{
+        await addTask(task)
+    }
+})
+
+
+async function addTask(task){
+    try{
+
+        const res = await fetch('http://localhost:3000/addtask',{
+            method:"POST",
+            headers:{'Content-Type':"application/json"},
+            body:JSON.stringify({task})
+        })
+        const data = await res.json()
+        if(res.status==201){
+            getdata()
+            document.getElementById("task").value=""
+            alert("Task Added")
+        }
+        else{
+            alert(data.error)
+        }
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+async function updateTask(id,updated_task) {
+    try{
+        const response = await fetch(`http://localhost:3000/update/${id}`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ task: updated_task }),
+        });
+        if (response.ok) {
+            alert("Task Updated");
+            document.getElementById("task").value=""
+            getdata();
+        }
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+
+
+async function deleteTask(id) {
+    
+    const res = await fetch(`http://localhost:3000/delete/${id}`)
+    const data = res.json()
+    if(res.status==200){
+        getdata()
+        alert("Task Deleted")
+    } else {
+        alert(data.error)
+    }
+  }
+
+
+
+
+
+
+
+
+//   document.getElementById("form").addEventListener("submit",async(e)=> {
+    //     e.preventDefault();
+    //     console.log("inside....")
+    //     try{
+    //         console.log("inside");
+            
+    //         const task = document.getElementById("task").value;
+    //         console.log(document.getElementById("task").value);
+            
+    //         const res = await fetch("/addtask",{
+    //             method:"POST",
+    //             headers:{"Content-Type":"application/json"},
+    //             body:JSON.stringify({task})
+    //         })
+    //         console.log(res);
+            
+    //         console.log("after respond");
+            
+    //         const data = await res.json()
+    //         if(res.status == 201){
+    //             getdata();
+    //             document.getElementById("task").value="";
+    //             alert("task added");
+    //         }
+    //         else{
+    //             alert(data.error);
+    //         }
+    //     }
+    //     catch(error){
+    //         console.log(error);
+            
+    //     }
+    // })
+    
+
+
+
+
+
+
+
+// console.log("successfully received data");
+// async function getdata(){
+//     const res = await fetch("/getdata");
+//     const data = await res.json();
+//     console.log(data);
+
+//     let str = "";
+//     data.forEach(e => {
+//         str+=`
+//         <div class="list">
+//             <div class="task">${e.task}</div>
+//             <div class="btn">
+//                 <button class="button" type="submit" onclick="updatefn('${e._id}', '${e.task}')">Edit</button>
+//                 <a href="/delete/${e._id}"><button class="button">Delete</button></a>
+//             </div>
+//         </div>`
+//     })
+//     document.getElementById("menu").innerHTML=str;
+// }
+// getdata()
+
+// function updatefn(id,task){
+//     document.getElementById("task").value = task;
+//     document.getElementById("addbtn").textContent = "Update";
+//     const form = document.getElementById("form")
+//     form.action = `/update/${id}`;
+// }
--- server.js
+++ server.js
@@ -1,86 +1,165 @@
 import express from "express";
-import path from 'path';
-import { dirname,join } from "path";
-import url, { fileURLToPath } from "url";
-// import mongoose from 'mongoose';
-import { MongoClient, ObjectId } from "mongodb"
-
-const client = new MongoClient("mongodb://127.0.0.1:27017")
-let __dirname = dirname(fileURLToPath(import.meta.url));
+import connection from "./connection.js";
+import todoSchema from "./models/todo.model.js";
 
 const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
 app.use(express.json());
 
-let collection;
-
-async function connectDatabase(){
-    try {
-        await client.connect();
-        console.log("Database connected");
-        const db = client.db("tododb");
-        collection = db.collection("todo");
-        app.listen(6001,() => {
-            console.log("the server is running at http://localhost:6001");
-        })
-    }
-    catch (error){
-        console.log(error);
-    }
-}
-
-connectDatabase();
-
-// set 'public' folder as a static folder
-app.use(express.static(path.join(__dirname,"public")));
-
-// app.get("/",(req,res) => {
-//     res.sendFile(path.join(__dirname,"public","index.html"));
-
-app.use(express.urlencoded())
-
-app.post("/send-data",async(req,res) => {
+// add task
+app.post("/addtask",async(req,res) => {
     console.log(req.body);
-    await collection.insertOne(req.body);
-    res.status(200).sendFile(join(__dirname,"public","index.html"));
-})  
-
-app.get("/getdata",async(req,res) => {
-    const todo = await collection.find({}).toArray();
-    console.log(todo);
-    res.status(200).send(JSON.stringify(todo));
-})
-
-app.get("/update",async(req,res) => {
-    const todo = await collection.find({}).toArray();
-    console.log(todo);
-    res.status(200).send(JSON.stringify(todo));
-})
-
-// app.get("/getdata",async (req,res)=>{
-//     const data = await collection.find({}).toArray()
-//     res.send(JSON.stringify(data))
     
-// })
+    try{
+        const {task} = req.body;
+        if(!task)
+            return res.status(404).send({error:"task is required"});
+        const data = await todoSchema.create({task});
+        res.status(201).send(data);
+    }
+    catch(err){
+        res.status(500).send({error:err})
+    }
+});
 
-// //add data to databse section
-// app.post("/add-task", async (req, res) => {
-//     try {
-//         await collection.insertOne(req.body);
-//         res.status(200).send("Data Added Successfully");
-//     } catch (error) {
-//         res.status(500).send(error.message);
-//     }
-// });
+app.get("/tasks",async(req,res) => {
+
+    try{
+        
+        const data = await todoSchema.find();
+        res.status(200).send(data);
+        
+    }
+    catch(err){
+        res.status(500).send({error:err});
+    }
+});
+
+connection()
+.then(() => {
+    app.listen(port,() => {
+        console.log('server is running on http://localhost:3000');
+    })
+})
+.catch((err) => {
+    console.log(err);
+})
+
+
 
 app.post("/update/:id", async (req, res) => {
-    const { id } = req.params;
-    console.log(id);
-    await collection.updateOne({ _id: new ObjectId(id) }, { $set: req.body });
-    res.status(200).redirect("/")
+    try {
+        const { id } = req.params
+        let task = req.body.task
+        const data = await todoSchema.findByIdAndUpdate(
+            id,
+            {task},
+            {new:true}
+        )
+        res.status(200).send(data)
+    } catch(err) {
+        res.status(500).send({error: err})
+    }
 });
 
-app.get("/delete/:id", async (req, res) => {
-    const { id } = req.params
-    await collection.deleteOne({ _id: new ObjectId(id) })
-    res.status(200).redirect("/")
-});
+app.get('/delete/:id',async(req,res)=>{
+    try {
+        const { id } = req.params
+        const data = await todoSchema.findByIdAndDelete(
+            id,
+            {new:true}
+        )
+        res.status(200).send(data)
+    } catch(err) {
+        res.status(500).send({error: err})
+    }
+})
+
+// import express from "express";
+// import path from 'path';
+// import { dirname,join } from "path";
+// import url, { fileURLToPath } from "url";
+// // import mongoose from 'mongoose';
+// import { MongoClient, ObjectId } from "mongodb"
+
+// const client = new MongoClient("mongodb://127.0.0.1:27017")
+// let __dirname = dirname(fileURLToPath(import.meta.url));
+
+// const app = express();
+// app.use(express.json());
+
+// let collection;
+
+// async function connectDatabase(){
+//     try {
+//         await client.connect();
+//         console.log("Database connected");
+//         const db = client.db("tododb");
+//         collection = db.collection("todo");
+//         app.listen(6001,() => {
+//             console.log("the server is running at http://localhost:6001");
+//         })
+//     }
+//     catch (error){
+//         console.log(error);
+//     }
+// }
+
+// connectDatabase();
+
+// // set 'public' folder as a static folder
+// app.use(express.static(path.join(__dirname,"public")));
+
+// // app.get("/",(req,res) => {
+// //     res.sendFile(path.join(__dirname,"public","index.html"));
+
+// app.use(express.urlencoded())
+
+// app.post("/send-data",async(req,res) => {
+//     console.log(req.body);
+//     await collection.insertOne(req.body);
+//     res.status(200).sendFile(join(__dirname,"public","index.html"));
+// })  
+
+// app.get("/getdata",async(req,res) => {
+//     const todo = await collection.find({}).toArray();
+//     console.log(todo);
+//     res.status(200).send(JSON.stringify(todo));
+// })
+
+// app.get("/update",async(req,res) => {
+//     const todo = await collection.find({}).toArray();
+//     console.log(todo);
+//     res.status(200).send(JSON.stringify(todo));
+// })
+
+// // app.get("/getdata",async (req,res)=>{
+// //     const data = await collection.find({}).toArray()
+// //     res.send(JSON.stringify(data))
+    
+// // })
+
+// // //add data to databse section
+// // app.post("/add-task", async (req, res) => {
+// //     try {
+// //         await collection.insertOne(req.body);
+// //         res.status(200).send("Data Added Successfully");
+// //     } catch (error) {
+// //         res.status(500).send(error.message);
+// //     }
+// // });
+
+// app.post("/update/:id", async (req, res) => {
+//     const { id } = req.params;
+//     console.log(id);
+//     await collection.updateOne({ _id: new ObjectId(id) }, { $set: req.body });
+//     res.status(200).redirect("/")
+// });
+
+// app.get("/delete/:id", async (req, res) => {
+//     const { id } = req.params
+//     await collection.deleteOne({ _id: new ObjectId(id) })
+//     res.status(200).redirect("/")
+// });
